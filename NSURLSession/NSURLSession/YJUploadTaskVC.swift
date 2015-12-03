d023ff2fe92c3d71b87947e519064cbe6a9a9ac8
yangjun <yangjun@touzila.com>
2015-12-03 18:39:05 +0800

    NSURLSession 项目架构

---
+++ NSURLSession/NSURLSession/YJUploadTaskVC.swift
@@ -0,0 +1,35 @@
+//
+//  YJUploadTaskVC.swift
+//  NSURLSession
+//
+//  Created by yangjun on 15/12/3.
+//  Copyright © 2015年 阳君. All rights reserved.
+//
+
+import UIKit
+
+class YJUploadTaskVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
