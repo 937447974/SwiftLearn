79bbef040400bc736ffb35aa6fcdaffee1cbacfc
yangjun <yangjun@touzila.com>
2015-12-20 12:14:44 +0800

    UI -> UICollectionViewDataSource

--- UI/UI/YJCollectionViewDataSourceVC.swift
+++ UI/UI/YJCollectionViewDataSourceVC.swift
@@ -9,54 +9,55 @@
 import UIKit
 
 /// UICollectionViewDataSource
-class YJCollectionViewDataSourceVC: UIViewController {
+class YJCollectionViewDataSourceVC: UIViewController, UICollectionViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        let cell = UICollectionViewCell(frame: CGRectMake(0, 0, 20, 20))
-//        self.collectionView?.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
-//        let nib = UINib(nibName: "YJCollectionViewCell", bundle: nil)
-//                self.collectionView?.registerNib(nib, forCellWithReuseIdentifier: "cell")
-//        
-//        self.clearsSelectionOnViewWillAppear = false
-//        self.collectionView?.contentSize = CGSizeMake(100, 100)
-//        self.collectionView?.collectionViewLayout
-//        print(self.collectionView)
     }
 
     // MARK: - UICollectionViewDataSource
     // MARK: 分组
     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-        return 2
+        print(__FUNCTION__)
+        return 3
     }
     
     // MARK: 每一组有多少个cell
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 100
+        print(__FUNCTION__)
+        return 10
     }
     
     // MARK: 生成Cell
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        print(__FUNCTION__)
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath)
-        cell.backgroundColor = UIColor.redColor()
+        cell.backgroundColor = UIColor.grayColor()
+        if let label: UILabel = cell.viewWithTag(8) as? UILabel {
+            label.text = "\(indexPath.row)"
+            
+        }
         return cell
     }
     
     
     // 生成Header或Footer
-//    override func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
-//        var crView: UICollectionReusableView!
-//        if (kind == UICollectionElementKindSectionHeader) { //
-//            crView = collectionView.dequeueReusableSupplementaryViewOfKind(kind, withReuseIdentifier: "header", forIndexPath: indexPath)
-//            // 标题
-//            let label:UILabel? = crView.viewWithTag(8) as? UILabel
-//            label?.text = "group"
-//        } else {
-//            crView = collectionView.dequeueReusableSupplementaryViewOfKind(kind, withReuseIdentifier: "footer", forIndexPath: indexPath)
-//        }
-//        return crView
-//    }
+    func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
+        print(__FUNCTION__)
+        var crView: UICollectionReusableView!
+        if (kind == UICollectionElementKindSectionHeader) { // Header
+            crView = collectionView.dequeueReusableSupplementaryViewOfKind(kind, withReuseIdentifier: "header", forIndexPath: indexPath)
+            // 标题
+            if let label: UILabel = crView.viewWithTag(8) as? UILabel {
+                label.text = "\(indexPath.section) Section"
+            }
+            crView.backgroundColor = UIColor.redColor()
+        } else { // Footer
+            crView = collectionView.dequeueReusableSupplementaryViewOfKind(kind, withReuseIdentifier: "footer", forIndexPath: indexPath)
+            crView.backgroundColor = UIColor.greenColor()
+        }
+        return crView
+    }
     
     // MARK: 能否移动
     func collectionView(collectionView: UICollectionView, canMoveItemAtIndexPath indexPath: NSIndexPath) -> Bool {
@@ -66,6 +67,7 @@ class YJCollectionViewDataSourceVC: UIViewController {
     
     // MARK: 移动cell
     func collectionView(collectionView: UICollectionView, moveItemAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
+        print(__FUNCTION__)
         print(sourceIndexPath)
         print(destinationIndexPath)
     }
