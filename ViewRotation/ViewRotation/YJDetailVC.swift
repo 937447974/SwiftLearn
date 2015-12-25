9a9ea8a39f76fd4ae47740588ee14a6f1d6fad65
yangjun <yangjun@touzila.com>
2015-12-25 15:22:47 +0800

    ViewRotation -> 代码注释

--- ViewRotation/ViewRotation/YJDetailVC.swift
+++ ViewRotation/ViewRotation/YJDetailVC.swift
@@ -2,6 +2,9 @@
 //  YJDetailVC.swift
 //  ViewRotation
 //
+//  CSDN:http://blog.csdn.net/y550918116j
+//  GitHub:https://github.com/937447974/Blog
+//
 //  Created by yangjun on 15/12/25.
 //  Copyright © 2015年 阳君. All rights reserved.
 //
