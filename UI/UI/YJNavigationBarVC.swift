3145ea3aaccb9b2277fefed166e7fbda5e3c7279
yangjun <yangjun@touzila.com>
2016-01-08 17:25:09 +0800

    UI -> 自定义UINavigationBar

--- UI/UI/YJNavigationBarVC.swift
+++ UI/UI/YJNavigationBarVC.swift
@@ -2,6 +2,9 @@
 //  YJNavigationBarVC.swift
 //  UI
 //
+//  CSDN:http://blog.csdn.net/y550918116j
+//  GitHub:https://github.com/937447974/Blog
+//
 //  Created by yangjun on 16/1/8.
 //  Copyright © 2016年 阳君. All rights reserved.
 //
@@ -13,19 +16,20 @@ class YJNavigationBarVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let bar = UINavigationBar.appearance()
-        bar.tintColor = UIColor.blackColor() // 按钮颜色
-        bar.barTintColor = UIColor.yellowColor()// 背景色
-        // 标题样式
-        bar.titleTextAttributes = [NSForegroundColorAttributeName:UIColor.blackColor()];
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        // 在新界面展示效果
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let vc = storyboard.instantiateInitialViewController()
-        self.navigationController?.presentViewController(vc!, animated: true, completion: nil)
+        // 自定义UINavigationBar
+        if let bar = self.navigationController?.navigationBar { // 共享bar UINavigationBar.appearance()
+            bar.tintColor = UIColor.blackColor() // 按钮颜色
+            bar.barTintColor = UIColor.yellowColor()// 背景色
+            bar.titleTextAttributes = [NSForegroundColorAttributeName:UIColor.blackColor()]; // 标题样式
+            bar.translucent = false // 是否透明
+            // 去掉UINavigationBar底部的黑边
+            bar.setBackgroundImage(UIImage(), forBarMetrics: UIBarMetrics.Default)
+            bar.shadowImage = UIImage()
+        }
     }
     
 }
