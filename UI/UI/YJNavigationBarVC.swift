642e79a15f5b48f1d2c98812b3669dc40ec39038
yangjun <yangjun@touzila.com>
2016-01-08 15:17:34 +0800

    UI -> Bar

--- UI/UI/YJNavigationBarVC.swift
+++ UI/UI/YJNavigationBarVC.swift
@@ -13,9 +13,19 @@ class YJNavigationBarVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let bar = UINavigationBar.appearance()
+        bar.tintColor = UIColor.blackColor() // 按钮颜色
+        bar.barTintColor = UIColor.yellowColor()// 背景色
+        // 标题样式
+        bar.titleTextAttributes = [NSForegroundColorAttributeName:UIColor.blackColor()];
     }
-
     
-
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        // 在新界面展示效果
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateInitialViewController()
+        self.navigationController?.presentViewController(vc!, animated: true, completion: nil)
+    }
+    
 }
