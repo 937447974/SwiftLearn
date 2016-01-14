d973b8c86a139e05726f3be9ddf3ff2818fe48b7
yangjun <yangjun@touzila.com>
2016-01-14 17:00:09 +0800

    Contact -> ContactsUI库加载

---
+++ Contact/Contact/YJContactsUIVC.swift
@@ -0,0 +1,37 @@
+//
+//  YJContactsUIVC.swift
+//  Contact
+//
+//  CSDN:http://blog.csdn.net/y550918116j
+//  GitHub:https://github.com/937447974/Blog
+//
+//  Created by yangjun on 16/1/14.
+//  Copyright © 2016年 阳君. All rights reserved.
+//
+
+import UIKit
+
+/// ContactsUI显示
+class YJContactsUIVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Action
+    // MARK: CNContactPickerViewController 测试
+    @IBAction func onClickCNContactPickerViewController(sender: AnyObject) {
+        
+    }
+    
+    // MARK: CNContactViewController 测试
+    @IBAction func onClickCNContactViewController(sender: AnyObject) {
+        
+    }
+    
+}
