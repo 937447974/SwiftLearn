23d5ddb30021257c5838076de981c91e3b0a84f7
yangjun <yangjun@touzila.com>
2016-01-14 19:43:14 +0800

    Contact -> ContactsUI介绍

--- Contact/Contact/YJContactsUIVC.swift
+++ Contact/Contact/YJContactsUIVC.swift
@@ -10,9 +10,10 @@
 //
 
 import UIKit
+import ContactsUI
 
 /// ContactsUI显示
-class YJContactsUIVC: UIViewController {
+class YJContactsUIVC: UIViewController, CNContactPickerDelegate, CNContactViewControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,12 +27,37 @@ class YJContactsUIVC: UIViewController {
     // MARK: - Action
     // MARK: CNContactPickerViewController 测试
     @IBAction func onClickCNContactPickerViewController(sender: AnyObject) {
-        
+        // 选择联系人或联系人的属性（如电话号码）
+        let vc = CNContactPickerViewController()
+        vc.delegate = self // 根据实现的代理方法指定单选、多选
+        self.presentViewController(vc, animated: true, completion: nil)
     }
     
     // MARK: CNContactViewController 测试
     @IBAction func onClickCNContactViewController(sender: AnyObject) {
-        
+        // 创建或修改联系人
+        let vc = CNContactViewController(forNewContact: nil)
+        vc.delegate = self
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    // MARK: - CNContactPickerDelegate
+    func contactPicker(picker: CNContactPickerViewController, didSelectContact contact: CNContact) {
+        // 单选
+        print(contact)
+    }
+    
+    // MARK: - CNContactViewControllerDelegate
+    func contactViewController(viewController: CNContactViewController, shouldPerformDefaultActionForContactProperty property: CNContactProperty) -> Bool {
+        print(__FUNCTION__)
+        print(property)
+        return true
+    }
+    
+    func contactViewController(viewController: CNContactViewController, didCompleteWithContact contact: CNContact?) {
+        viewController.navigationController?.popViewControllerAnimated(true)
+        print(__FUNCTION__)
+        print(contact)
     }
     
 }
