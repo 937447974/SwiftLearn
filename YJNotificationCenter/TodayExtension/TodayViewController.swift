47c680d7bad713eb6109ec904c4d7b76790358c7
yangjun <yangjun@touzila.com>
2016-01-19 17:01:52 +0800

    YJNotificationCenter -> 扩展实现

---
+++ YJNotificationCenter/TodayExtension/TodayViewController.swift
@@ -0,0 +1,70 @@
+//
+//  TodayViewController.swift
+//  TodayExtension
+//
+//  Created by yangjun on 16/1/19.
+//  Copyright © 2016年 阳君. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+    
+    @IBOutlet weak var button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view from its nib.
+        // 隐藏或显示界面
+        NCWidgetController.widgetController().setHasContent(true, forWidgetWithBundleIdentifier: YJUtilsAPP.identifier)
+        print(YJUtilsAPP.identifier)
+        // 调整显示区域
+        self.preferredContentSize = CGSizeMake(self.view.frame.width, 50)
+        print(self.view.frame)
+    }
+    
+    deinit {
+        print("内存回收")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onClickButton(sender: AnyObject) {
+        print(__FUNCTION__)
+        // 数据同步
+        if let user = NSUserDefaults(suiteName: "group.com.YJNotificationCenter") {
+            print(user.objectForKey("test"))
+            user.setObject("阳君测试", forKey: "test")
+            print(user.synchronize())
+        }
+        // 打开当前应用
+        if let url = NSURL(string: "appextension://test") {
+            self.extensionContext?.openURL(url, completionHandler: { (success: Bool) -> Void in
+                print("open url result: \(success)")
+            })
+        }
+    }
+    
+    // MARK: - NCWidgetProviding
+    // MARK: 当前状态
+    func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        print(__FUNCTION__)
+        completionHandler(NCUpdateResult.NewData)
+    }
+    
+    // MARK: 扩展显示边缘
+    func widgetMarginInsetsForProposedMarginInsets(defaultMarginInsets: UIEdgeInsets) -> UIEdgeInsets {
+        print(__FUNCTION__)
+        print(defaultMarginInsets)
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
+}
--- YJNotificationCenter/YJNotificationCenter/AppDelegate.swift
+++ YJNotificationCenter/YJNotificationCenter/AppDelegate.swift
@@ -16,30 +16,50 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        print(__FUNCTION__)
         return true
     }
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        print(__FUNCTION__)
     }
 
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        print(__FUNCTION__)
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        print(__FUNCTION__)
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        print(__FUNCTION__)
+        if let user = NSUserDefaults(suiteName: "group.com.YJNotificationCenter") {
+            print(user.objectForKey("test"))
+            user.setObject("通知扩展", forKey: "test")
+            user.synchronize()
+        }
     }
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        print(__FUNCTION__)
     }
+    
+    // MARK: - 能否打开应用
+    func application(app: UIApplication, openURL url: NSURL, options: [String : AnyObject]) -> Bool {
+        print(url)
+        print(options)
+        return true
+    }
+    
+    
 
 
 }
