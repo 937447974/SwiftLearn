790dbcb79913301ae789f715df21eae46992639a
yangjun <yangjun@touzila.com>
2016-01-21 11:43:10 +0800

    SafariServices -> 框架测试

--- YJSafariServices/YJSafariServices/ViewController.swift
+++ YJSafariServices/YJSafariServices/ViewController.swift
@@ -2,24 +2,88 @@
 //  ViewController.swift
 //  YJSafariServices
 //
+//  CSDN:http://blog.csdn.net/y550918116j
+//  GitHub:https://github.com/937447974/Blog
+//
 //  Created by yangjun on 16/1/21.
 //  Copyright © 2016年 阳君. All rights reserved.
 //
 
 import UIKit
+import SafariServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SFSafariViewControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: - SFContentBlockerManager
+    func testSFContentBlockerManager() {
+        // SFContentBlockerManager 测试
+        SFContentBlockerManager.reloadContentBlockerWithIdentifier(YJUtilsAPP.identifier) { (error: NSError?) -> Void in
+            if error != nil {
+                print(error)
+            }
+        }
     }
 
+    // MARK: - SSReadingList {
+    func testSSReadingList() {
+        // SSReadingList测试
+        guard let url = NSURL(string: "https://www.baidu.com") else { // 生成url
+            return
+        }
+        guard SSReadingList.supportsURL(url) else { // 是否支持添加该链接
+            return
+        }
+        do {
+            // 添加到阅读列表
+            try SSReadingList.defaultReadingList()?.addReadingListItemWithURL(url, title: "title", previewText: "previewText")
+        } catch SSReadingListErrorCode.URLSchemeNotAllowed {
+            print(SSReadingListErrorDomain)
+        } catch {
+            print(error)
+        }
+    }
 
+    // MARK: - SFSafariViewController
+    @IBAction func onClickButton(sender: AnyObject) {
+        guard let url = NSURL(string: "https://developer.apple.com/library/ios/documentation/SafariServices/Reference/SFSafariViewController_Ref/index.html") else {
+            return
+        }
+        let vc = SFSafariViewController(URL: url, entersReaderIfAvailable: true)
+        vc.delegate = self
+        self.presentViewController(vc, animated: true, completion: nil)
+    }
+
+    // MARK: - SFSafariViewControllerDelegate
+    // MARK: web页面加载完成
+    func safariViewController(controller: SFSafariViewController, didCompleteInitialLoad didLoadSuccessfully: Bool) {
+        print(__FUNCTION__)
+        print(didLoadSuccessfully)
+    }
+    
+    func safariViewController(controller: SFSafariViewController, activityItemsForURL URL: NSURL, title: String?) -> [UIActivity] {
+        print(__FUNCTION__)
+        print("\(URL)--\(title)")
+        return [YJUIActivity()]
+    }
+    
+    // MARK: 退出页面
+    func safariViewControllerDidFinish(controller: SFSafariViewController) {
+        print(__FUNCTION__)
+    }
+    
+}
+
+/// 分享按钮
+private class YJUIActivity: UIActivity {
+    
+    private override func activityTitle() -> String? {
+        return "阳君"
+    }
+    
 }
 
