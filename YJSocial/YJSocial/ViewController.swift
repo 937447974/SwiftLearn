73b0829b3bad3a917a4a273b610b08bcaaf2e763
yangjun <yangjun@touzila.com>
2016-01-25 15:49:36 +0800

    SLComposeServiceViewController主要用于在当前应用开启分享界面，快速分享相关内容到其他APP或社交平台

--- YJSocial/YJSocial/ViewController.swift
+++ YJSocial/YJSocial/ViewController.swift
@@ -10,24 +10,37 @@
 //
 
 import UIKit
+import Social
 
+/// 快速分享
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
-        print(self.extensionContext?.inputItems)
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        super.touchesBegan(touches, withEvent: event)
+        // 分享到当前应用扩展（微博分享SLServiceTypeSinaWeibo）
+        let serviceType = "com.YJSocial.ShareExtension" // 扩展Bundle identifier
+        guard SLComposeViewController.isAvailableForServiceType(serviceType) else {
+            print("不支持:\(serviceType)")
+            return
+        }
+        let vc = SLComposeViewController(forServiceType: serviceType)
+        vc.setInitialText(serviceType) // 默认内容
+        // 处理结果回调
+        vc.completionHandler =  {(result: SLComposeViewControllerResult) -> Void in
+            switch result {
+            case SLComposeViewControllerResult.Cancelled:
+                print("Cancelled")
+            case SLComposeViewControllerResult.Done:
+                print("Done")
+            }
+        }
+        self.presentViewController(vc, animated: true, completion: nil)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
