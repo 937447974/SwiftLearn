024f88c0a93d11bf07d9c047857935ff2a3e797c
yangjun <yangjun@touzila.com>
2016-01-26 17:22:58 +0800

    CoreMotion -> 项目启动

---
+++ YJCoreMotion/YJCoreMotion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YJCoreMotion
+//
+//  Created by yangjun on 16/1/26.
+//  Copyright © 2016年 阳君. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ YJCoreMotion/YJCoreMotionTests/YJCoreMotionTests.swift
@@ -0,0 +1,36 @@
+//
+//  YJCoreMotionTests.swift
+//  YJCoreMotionTests
+//
+//  Created by yangjun on 16/1/26.
+//  Copyright © 2016年 阳君. All rights reserved.
+//
+
+import XCTest
+@testable import YJCoreMotion
+
+class YJCoreMotionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
