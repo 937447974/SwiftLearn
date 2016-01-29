e6c637046f961fec08c06b822fbebf907591ccd3
yangjun <yangjun@touzila.com>
2016-01-29 10:20:47 +0800

    CoreTelephony -> 项目启动

---
+++ YJCoreTelephony/YJCoreTelephony/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YJCoreTelephony
+//
+//  Created by yangjun on 16/1/29.
+//  Copyright © 2016年 阳君. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
