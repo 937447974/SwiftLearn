a107a5ce30f42ddb8efd0239cc79ff7895aa7c66
yangjun <yangjun@touzila.com>
2016-01-30 19:39:36 +0800

    Foundation -> NSThread演示

--- YJFoundation/YJFoundation/YJThreadVC.swift
+++ YJFoundation/YJFoundation/YJThreadVC.swift
@@ -12,24 +12,25 @@ class YJThreadVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        guard NSThread.isMultiThreaded() else {
+            print("设备不支持多线程")
+            return
+        }
+        // 方式一
+        let thread = NSThread(target: self, selector: "send:", object: "阳君")
+        print("线程开销：\(thread.stackSize)bytes")
+        thread.start() // 执行
+        
+        // 方式二
+        NSThread.detachNewThreadSelector("send:", toTarget: self, withObject: "937447974")
+        
+        // 方式三
+        // 参考协议NSObjectProtocol
+        self.performSelector("send:", withObject: "直接发送")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func send(str: String) {
+        print("是否主线程：\(NSThread.isMainThread()),接受数据：\(str)")
     }
-    */
 
 }
