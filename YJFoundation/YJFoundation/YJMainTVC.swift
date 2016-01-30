b03f352a1b0935aadb58fa2741f2dcd62339b207
yangjun <yangjun@touzila.com>
2016-01-30 18:46:20 +0800

    Foundation -> 引导界面搭建

--- YJFoundation/YJFoundation/YJMainTVC.swift
+++ YJFoundation/YJFoundation/YJMainTVC.swift
@@ -2,6 +2,9 @@
 //  YJMainTVC.swift
 //  YJFoundation
 //
+//  CSDN:http://blog.csdn.net/y550918116j
+//  GitHub:https://github.com/937447974/Blog
+//
 //  Created by yangjun on 16/1/30.
 //  Copyright © 2016年 阳君. All rights reserved.
 //
@@ -11,4 +14,14 @@ import UIKit
 /// 主界面
 class YJMainTVC: YJBaseTVC {
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var list: [YJPerformSegueModel]!
+        
+        list = [YJPerformSegueModel]()
+        list.append(YJPerformSegueModel(title: "NSThread"){YJThreadVC()})
+        self.data.append(list)
+        self.header.append("Threed")
+    }
 }
