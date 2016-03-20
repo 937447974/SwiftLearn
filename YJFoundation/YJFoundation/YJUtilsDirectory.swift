c6814703d25945cd355b4a460df1eb010945ed47
阳君 <937447974@qq.com>
2016-03-20 11:39:15 +0800

    Foundation -> 完成获取应用内目录的工具类

---
+++ YJFoundation/YJFoundation/YJUtilsDirectory.swift
@@ -0,0 +1,41 @@
+//
+//  YJUtilsDirectory.swift
+//  YJFoundation
+//
+//  CSDN:http://blog.csdn.net/y550918116j
+//  GitHub:https://github.com/937447974/Blog
+//
+//  Created by admin on 16/3/20.
+//  Copyright © 2016年 阳君. All rights reserved.
+//
+
+import UIKit
+
+/// app应用目录
+public struct YJUtilsDirectory {
+    
+    // MARK: - path路径
+    /// HomeDirectoryPath
+    static let homePath = NSHomeDirectory()
+    /// DocumentDirectoryPath
+    static let documentPath = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true).first!
+    /// LibraryDirectoryPath
+    static let libraryPath = NSSearchPathForDirectoriesInDomains(.LibraryDirectory, .UserDomainMask, true).first!
+    /// CachesDirectoryPath
+    static let cachesPath = NSSearchPathForDirectoriesInDomains(.CachesDirectory, .UserDomainMask, true).first!
+    /// TemporaryDirectoryPath
+    static let tempPath = NSTemporaryDirectory()
+    
+    // MARK: - url 路径
+    /// HomeDirectoryURL
+    static let homeURL = NSURL(fileURLWithPath: YJUtilsDirectory.homePath)
+    /// DocumentDirectoryURL
+    static let documentURL = NSURL(fileURLWithPath: YJUtilsDirectory.homePath)
+    /// LibraryDirectoryURL
+    static let libraryURL = NSURL(fileURLWithPath: YJUtilsDirectory.libraryPath)
+    /// CachesDirectoryURL
+    static let cachesURL = NSURL(fileURLWithPath: YJUtilsDirectory.cachesPath)
+    /// TemporaryDirectoryURL
+    static let tempURL = NSURL(fileURLWithPath: YJUtilsDirectory.tempPath)
+    
+}
